39b9354a4b9b92928c8bb8bfdec239396fb804f1
zynska <zynska.18@gmail.com>
2026-04-29 21:05:38 +0800

    character update

--- View/Scenes/CharacterSelection.py
+++ View/Scenes/CharacterSelection.py
@@ -10,6 +10,7 @@ class CharacterSelection:
         self.scene_manager = scene_manager
         font_path = os.path.join("assets", "font", "VCR_OSD_MONO_1.001.ttf")
         self.font = pygame.font.Font(font_path, 48)
+        self.small_font = pygame.font.Font(font_path, 32)  # smaller font for labels
 
         # Load full background
         self.background = pygame.image.load("assets/scenery/plain_bg.png").convert()
@@ -32,16 +33,16 @@ class CharacterSelection:
         self.girl_image = pygame.image.load("assets/characters/girl_char.png").convert_alpha()
         self.boy_image = pygame.image.load("assets/characters/boy_char.png").convert_alpha()
 
-        # Define available characters
-        self.characters = [
-            {"label": "GIRL", "id": "girl"},
-            {"label": "BOY", "id": "boy"},
-        ]
-
-        self.selected_index = None
         self.chosen_character = None
 
         self._create_layout()
+
+    def _scale_image_proportionally(self, image, box_size):
+        """Scale image proportionally to fit inside box without stretching."""
+        img_w, img_h = image.get_size()
+        scale = min((box_size - 20) / img_w, (box_size - 20) / img_h)
+        new_size = (int(img_w * scale), int(img_h * scale))
+        return pygame.transform.smoothscale(image, new_size)
 
     def _create_layout(self):
         screen_width, screen_height = self.screen.get_size()
@@ -52,7 +53,7 @@ class CharacterSelection:
         self.window_scaled = pygame.transform.smoothscale(self.window_image, (square_size, square_size))
         self.window_rect = self.window_scaled.get_rect(center=(screen_width // 2, screen_height // 2))
 
-        # Scale buttons
+        # Buttons inside window
         btn_width = square_size // 3
         btn_height = int(btn_width * 0.4)
         self.back_btn = pygame.transform.smoothscale(self.back_btn_original, (btn_width, btn_height))
@@ -65,13 +66,13 @@ class CharacterSelection:
 
         # Character boxes inside window
         box_size = square_size // 3
-        box_y = self.window_rect.top + 150
+        box_y = self.window_rect.top + 120
         self.girl_box = pygame.Rect(self.window_rect.centerx - box_size - 50, box_y, box_size, box_size)
         self.boy_box = pygame.Rect(self.window_rect.centerx + 50, box_y, box_size, box_size)
 
-        # Scale character images to fit boxes
-        self.girl_image_scaled = pygame.transform.smoothscale(self.girl_image, (box_size - 20, box_size - 20))
-        self.boy_image_scaled = pygame.transform.smoothscale(self.boy_image, (box_size - 20, box_size - 20))
+        # Scale character images proportionally
+        self.girl_image_scaled = self._scale_image_proportionally(self.girl_image, box_size)
+        self.boy_image_scaled = self._scale_image_proportionally(self.boy_image, box_size)
 
     def draw(self):
         self.screen.blit(self.background_scaled, (0, 0))
@@ -79,17 +80,37 @@ class CharacterSelection:
 
         mouse_pos = pygame.mouse.get_pos()
 
-        # Header text
-        header_surface = self.font.render("SELECT YOUR EXPLORER", True, (255, 255, 255))
-        header_rect = header_surface.get_rect(center=(self.window_rect.centerx, self.window_rect.top + 60))
+        # Header text (resize if too wide)
+        header_text = "SELECT YOUR EXPLORER"
+        max_width = self.window_rect.width - 40
+        header_surface = self.font.render(header_text, True, (255, 255, 255))
+        if header_surface.get_width() > max_width:
+            smaller_font = pygame.font.Font(os.path.join("assets", "font", "VCR_OSD_MONO_1.001.ttf"), 36)
+            header_surface = smaller_font.render(header_text, True, (255, 255, 255))
+        header_rect = header_surface.get_rect(center=(self.window_rect.centerx, self.window_rect.top + 50))
         self.screen.blit(header_surface, header_rect)
 
-        # Draw character boxes
-        for box, image in [(self.girl_box, self.girl_image_scaled), (self.boy_box, self.boy_image_scaled)]:
-            pygame.draw.rect(self.screen, (25, 16, 21), box)  # fill color #191015
-            pygame.draw.rect(self.screen, (0, 0, 0), box, 2)  # thin black border
+        # Draw character boxes + labels
+        for box, image, char_id, label_lines in [
+            (self.girl_box, self.girl_image_scaled, "girl", ["FEMALE", "EXPLORER"]),
+            (self.boy_box, self.boy_image_scaled, "boy", ["MALE", "EXPLORER"])
+        ]:
+            pygame.draw.rect(self.screen, (92, 64, 52), box)  # fill color #5C4034
+            border_color = (255, 215, 0) if self.chosen_character == char_id else (0, 0, 0)
+            if box.collidepoint(mouse_pos):  # hover effect
+                border_color = (173, 216, 230)
+            pygame.draw.rect(self.screen, border_color, box, 2)
+
+            # Center character image
             img_rect = image.get_rect(center=box.center)
             self.screen.blit(image, img_rect)
+
+            # Render each line of label below the box
+            line_spacing = 35
+            for i, line in enumerate(label_lines):
+                label_surface = self.small_font.render(line, True, (255, 255, 255))
+                label_rect = label_surface.get_rect(center=(box.centerx, box.bottom + 30 + i * line_spacing))
+                self.screen.blit(label_surface, label_rect)
 
         # Back button
         if self.back_btn_rect.collidepoint(mouse_pos):
